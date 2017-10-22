1beaa3e0b2f141b5d7c29a9ceb01cbd7fd1df78b
HLA-systeem <aaakanten@outlook.com>
2017-10-22 22:01:09 +0200

    forgot to press save

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,22 +1,23 @@
 window.addEventListener('load', function(){
     var req = new XMLHttpRequest();
+    if(document.getElementById('public') != null){
+        document.getElementById('public').addEventListener('click', function(){
+            var publicValue = document.getElementById('public').checked; 
 
-    document.getElementById('public').addEventListener('click', function(){
-        var publicValue = document.getElementById('public').checked; 
-
-        req.onreadystatechange = function(){
-            if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
-            }
-        };
-        req.open('POST', sendTo, true); //last two user & pass
-        req.setRequestHeader('X-CSRF-TOKEN', token)
-       
-        var data = new FormData();
-        data.append('value', publicValue);
-        data.append('id', id);
-        req.send(data);
-    });
+            req.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                }
+            };
+            req.open('POST', sendTo, true); //last two user & pass
+            req.setRequestHeader('X-CSRF-TOKEN', token)
+        
+            var data = new FormData();
+            data.append('value', publicValue);
+            data.append('id', id);
+            req.send(data);
+        });
+    }
 
     document.getElementById('searchBar').addEventListener('onkeyup', function(){
         var q = document.getElementById('searchBar').value; 
